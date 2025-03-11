83209f0652986ad6fb24a7e6ca74d598972f53b1
JohnniePro <toshigamer21@gmail.com>
2025-03-11 09:33:32 +0300

    added setup for database layer

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,6 +1,10 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from fastapi import Depends
+from .setup import initialize_database  
+
+# Run database setup on first application start
+initialize_database()
 
 
 def get_db():
