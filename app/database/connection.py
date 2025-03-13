cd6bd7064a5a161bee7d7424f089bdcc2689d35a
JohnniePro <toshigamer21@gmail.com>
2025-03-13 10:52:32 +0300

    add enviromental variables

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,17 +1,30 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from fastapi import Depends
-from .setup import initialize_database  
+from .setup import initialize_database
+import os
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
+
+# Retrieve environment variables
+db_username = os.getenv('DATABASE_USERNAME')
+db_password = os.getenv('DATABASE_PASSWORD')
+db_host = os.getenv('DATABASE_HOST')
+db_port = os.getenv('DATABASE_PORT')
+db_name = os.getenv('DATABASE_NAME')
 # Run database setup on first application start
 initialize_database()
 
 
 def get_db():
     db = psycopg2.connect(
-        dbname="face_recognition",
-        user="postgres",
-        password="postgress",
+        dbname=db_name,
+        user=db_username,
+        password=db_password,
+        host=db_host,
+        port=db_port,
         cursor_factory=RealDictCursor
     )
     try:
